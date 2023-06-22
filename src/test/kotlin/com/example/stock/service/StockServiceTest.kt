b2e2780048b7b6b1231b1f7cc6031bc404b23b60
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2023-06-23 00:17:32 +0900

    feat: 동시에 100건의 결제건 테스트

--- src/test/kotlin/com/example/stock/service/StockServiceTest.kt
+++ src/test/kotlin/com/example/stock/service/StockServiceTest.kt
@@ -2,14 +2,15 @@ package com.example.stock.service
 
 import com.example.stock.domain.Stock
 import com.example.stock.repository.StockRepository
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 @SpringBootTest
 class StockServiceTest @Autowired constructor(
@@ -34,6 +35,28 @@ class StockServiceTest @Autowired constructor(
 
         val stock = stockRepository.findById(1L).orElseThrow()
 
-        assertThat(stock.getQuantity()).isEqualTo(99)
+        assertThat(stock.quantity).isEqualTo(99)
+    }
+
+    @Test
+    fun 동시에_100개의_요청() {
+        val threadCount: Int = 100
+        val executorService: ExecutorService = Executors.newFixedThreadPool(32)
+        val latch: CountDownLatch = CountDownLatch(threadCount)
+
+        for (i in 1..threadCount) {
+            executorService.submit {
+                try {
+                    stockService.decrease(1L, 1L)
+                } finally {
+                    latch.countDown()
+                }
+            }
+        }
+        latch.await()
+
+        val stock = stockRepository.findById(1L).orElseThrow()
+
+        assertThat(stock.quantity).isEqualTo(0L)
     }
 }
